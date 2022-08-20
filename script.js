215e246a6167e0a11e93d02d08a09843896a4a85
Denysmak <denysmakene13@gmail.com>
2022-08-20 00:28:41 -0300

    hover nos icones de perfil

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-const usuario = document.querySelectorAll(".usuario");
+const barbosa = document.getElementById("barbosa");
+const igor = document.getElementById("igor");
+const denys = document.getElementById("denys");
+const joana = document.getElementById("joana");
+
+
+
+
+let hoverMaior = (e) => {
+    e.classList.remove("perfilMenor")
+    e.classList.add("perfilMaior")
+
+    e.parentElement.parentElement.children[1].classList.remove("fontMenor")
+    e.parentElement.parentElement.children[1].classList.add("fontMaior")
+    e.parentElement.parentElement.children[1].style.fontSize = "1.2rem";
+}
+let hoverMenor = (e) => {
+    e.classList.remove("perfilMaior")
+    e.classList.add("perfilMenor")
+    
+    
+    e.parentElement.parentElement.children[1].classList.remove("fontMaior")
+    e.parentElement.parentElement.children[1].classList.add("fontMenor")
+    e.parentElement.parentElement.children[1].style.fontSize = "1rem";
+}
+
+
 
